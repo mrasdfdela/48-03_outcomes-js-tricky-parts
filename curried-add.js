34c369d09be0cc91e7466a66964c46f82c9a5819
Jerry Hsu <Jerry>
2021-11-01 18:01:26 -0400

    curried add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,13 @@
 function curriedAdd(total) {
-
+  if (!total) return 0;
+  return function innerAdd(y){
+    if(y){
+      total += y;
+      return innerAdd
+    } else {
+      return total;
+    }
+  }
 }
 
 module.exports = { curriedAdd };
--- outcomes-js-tricky-parts-solution/curried-add.test.js
+++ outcomes-js-tricky-parts-solution/curried-add.test.js
@@ -1,13 +1,13 @@
 const { curriedAdd } = require("./curried-add");
 
 describe("curriedAdd", function() {
-  // it("returns 0 when called with no arguments", function() {
-  //   expect(curriedAdd()).toBe(0);
-  // });
+  it("returns 0 when called with no arguments", function() {
+    expect(curriedAdd()).toBe(0);
+  });
 
-  // it("returns a function when called with an argument", function() {
-  //   expect(curriedAdd(3)).toBeInstanceOf(Function);
-  // });
+  it("returns a function when called with an argument", function() {
+    expect(curriedAdd(3)).toBeInstanceOf(Function);
+  });
 
   it("accumulates a total with repeated function calls", function() {
     expect(curriedAdd(1)()).toBe(1);
