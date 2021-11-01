a3c5e7c2654d21a07c6b020eec79cf4730f2d76a
Jerry Hsu <Jerry>
2021-11-01 16:53:53 -0400

    Closure exercises; guessing game & bank account

--- create-account.js
+++ create-account.js
@@ -1,5 +1,37 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount=0) {
+  let userPin = pin;
+  let userAmt = amount;
+  return {
+    checkBalance(pin){
+      return pin === userPin ? `$${userAmt}` : "Invalid PIN."
+    },
+    deposit(pin,depAmt){
+      if (pin === userPin){
+        userAmt += depAmt
+        return `Succesfully deposited $${depAmt}. Current balance: $${userAmt}.`;
+      } else {
+        return "Invalid PIN."
+      }
+    },
+    withdraw(pin, wdwlAmt) {
+      if (pin !== userPin){
+        return "Invalid PIN."
+      } else if (pin === userPin && wdwlAmt < userAmt) {
+        userAmt -= wdwlAmt;
+        return `Succesfully withdrew $${wdwlAmt}. Current balance: $${userAmt}.`;
+      } else {
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      }
+    },
+    changePin(pin, newPin){
+      if (pin === userPin){
+        userPin = newPin;
+        return "PIN successfully changed!";
+      } else {
+        return "Invalid PIN.";
+      }
+    }
+  }
 }
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
+  const secretNum = Math.floor(Math.random() * 100);
+  let gameOver = false;
+  let guessCount = 0;
 
+  return function evalGuess(guessNum=undefined) {
+    if (!guessNum) return;
+    if (gameOver) return "The game is over, you already won!";
+    guessCount++;
+    
+    if (secretNum < guessNum) {
+      return `${guessNum} is too high!`;
+    } else if (secretNum > guessNum) {
+      return `${guessNum} is too low!`;
+    } else if (secretNum === guessNum) {
+      gameOver = true;
+      return `You win! You found ${secretNum} in ${guessCount} guesses.`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
